fa1abbfee755a1ee873c3da62c4739220b5bfdf8
Sumit Kumar <sumitchavan1002@gmail.com>
2022-08-26 10:00:05 +0530

    Initial commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	// create a rest end point using which we create shorter urls and also retrieve original urls based on shorter
+}
