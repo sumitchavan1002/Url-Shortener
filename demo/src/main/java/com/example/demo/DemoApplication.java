80bc623adb9d3472a872df60255c919dee92e62e
Sumit Kumar <sumitchavan1002@gmail.com>
2024-10-24 17:59:16 +0530

    added commedt

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	// create a rest end point using which we create shorter urls and also retrieve original urls based on shorter
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 	// create a rest end point using which we create shorter urls and also retrieve original urls based on shorter
 }
