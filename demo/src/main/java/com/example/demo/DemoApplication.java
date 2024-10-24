b953a3feedb9d3e9dd7675b291e04307fdd057eb
Sumit Kumar <sumitchavan1002@gmail.com>
2024-10-24 18:14:32 +0530

    added comment

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	// create a rest end point using which we create shorter urls and also retrieve original urls based on shorter
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
